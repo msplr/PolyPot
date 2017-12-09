d9f0389e3041638940ea04a024936a5102215f51
Michael Spieler <michael.spieler@epfl.ch>
2017-12-10 00:58:33 +0100

    set moisture sensor gain, TOF sensor enable

--- module/src/sensors.py
+++ module/src/sensors.py
@@ -8,12 +8,16 @@ def sensor_pwr(enable):
     else:
         SENSOR_ENn.value(1)
 
-pin_moist = machine.Pin(35, machine.Pin.IN)
-pin_battery = machine.Pin(32, machine.Pin.IN)
-pin_luminosity = machine.Pin(33, machine.Pin.IN)
 
+pin_moist = machine.Pin(35, machine.Pin.IN)
 adc_moist = machine.ADC(pin_moist) # ADC1 CH 7
+adc_moist.atten(machine.ADC.ATTN_11DB)
+adc_moist.read()
+
+pin_battery = machine.Pin(32, machine.Pin.IN)
 adc_battery = machine.ADC(pin_battery) # ADC1 CH 4
+
+pin_luminosity = machine.Pin(33, machine.Pin.IN)
 adc_luminosity = machine.ADC(pin_luminosity) # ADC1 CH 5
 
 adc_luminosity.read()
@@ -28,3 +32,7 @@ temp = i2c.readfrom_mem(72, 0x00, 2)
 temp = struct.unpack('>h', temp) # MSByte first -> 16 bit big endian
 (temp, ) = temp # upack tuple
 temp = temp / 256
+
+# TOF distance sensor
+TOF_RESETn = machine.Pin(17, machine.Pin.OUT)
+TOF_RESETn.value(1) # enable TOF sensor
