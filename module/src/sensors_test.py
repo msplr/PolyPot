a1a802585a83bb4aff3cbe7b0814515c056be26e
Michael Spieler <michael.spieler@epfl.ch>
2017-12-14 01:26:50 +0100

    test TOF sensor notes

--- module/src/sensors_test.py
+++ module/src/sensors_test.py
@@ -34,5 +34,32 @@ temp = struct.unpack('>h', temp) # MSByte first -> 16 bit big endian
 temp = temp / 256
 
 # TOF distance sensor
+import machine
+import sensors
+SENSOR_ENn = machine.Pin(25, machine.Pin.OUT)
+SENSOR_ENn.value(0)
+
 TOF_RESETn = machine.Pin(17, machine.Pin.OUT)
 TOF_RESETn.value(1) # enable TOF sensor
+
+i2c = machine.I2C(scl=machine.Pin(16), sda=machine.Pin(4), freq=100000)
+i2c.scan()
+
+TOF_ADDR = 0x29
+i2c.readfrom_mem(TOF_ADDR, 0x000, 1, addrsize=16)
+i2c.writeto_mem(TOF_ADDR, 0x0011, b'\x10', addrsize=16)
+
+def rd(reg):
+    data = i2c.readfrom_mem(TOF_ADDR, reg, 1, addrsize=16)
+    (data, ) = struct.unpack('B', data)
+    return data
+
+def wr(reg, data):
+    data = struct.pack('B', data)
+    i2c.writeto_mem(TOF_ADDR, reg, data, addrsize=16)
+
+import sensors
+
+tof = sensors.VL6180X(i2c, TOF_RESETn)
+tof.start()
+tof.distance()
