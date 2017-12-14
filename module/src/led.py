047c9ad8ade7e7fe29a7feda3576fad7e8d8bb41
Michael Spieler <michael.spieler@epfl.ch>
2017-12-14 19:05:50 +0100

    update LED module

--- module/src/led.py
+++ module/src/led.py
@@ -1,4 +1,18 @@
 import machine
 
-g = machine.Pin(22, machine.Pin.OUT)
-r = machine.Pin(21, machine.Pin.OUT)
+led_green = machine.Pin(22, machine.Pin.OUT)
+led_red = machine.Pin(21, machine.Pin.OUT)
+
+def LED(status):
+    if status == "green":
+        led_green.value(1)
+        led_red.value(0)
+    elif status == "orange":
+        led_green.value(1)
+        led_red.value(1)
+    elif status == "red":
+        led_green.value(0)
+        led_red.value(1)
+    else:
+        led_green.value(0)
+        led_red.value(0)
