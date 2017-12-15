b448f64b6112dddd31c59c75859c64c444d915dc
niederha <33467654+niederha@users.noreply.github.com>
2017-12-15 23:34:11 +0100

    Cleaned and debbuged

--- module/src/communication.py
+++ module/src/communication.py
@@ -3,58 +3,53 @@ import urequests as requests
 import ujson as json
 import usocket as socket
 import time
-#TODO: use **kwarg to merge the functions
 
-#Re activates the AP
-def AP_activation(ap):
-    ap.active(True)
-    ap.config(essid="PolyPot", password="setupPolyPot", authmode=network.AUTH_WPA_WPA2_PSK)
+WIFI_TIMEOUT=5
 
-#Activates an AP. A WLAN object containig the active AP
-def AP_activation():
-
-    ap = network.WLAN(network.AP_IF)
-    ap.active(True)
-    ap.ifconfig(("192.168.1.1", "255.255.255.0", "192.168.4.1", "8.8.8.8"))
-    ap.config(essid="PolyPot", password="setupPolyPot", authmode=network.AUTH_WPA_WPA2_PSK)
-    return ap
+#Activates or reactivates the AP
+def AP_activation(ap=None):
+    if ap:
+        ap.active(True)
+        ap.config(essid="PolyPot", password="setupPolyPot", authmode=network.AUTH_WPA_WPA2_PSK)
+    else:
+        ap = network.WLAN(network.AP_IF)
+        ap.active(True)
+        ap.ifconfig(("192.168.1.1", "255.255.255.0", "192.168.4.1", "8.8.8.8"))
+        ap.config(essid="PolyPot", password="setupPolyPot", authmode=network.AUTH_WPA_WPA2_PSK)
+        return ap
 
 #Gets the wifi config in json format when the pot is in AP
 def get_post():
+    #Setup the socket
     addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
-
     s = socket.socket()
-
     s.bind(addr)
 
+    #Reciving the post request
     s.listen(1)
-
     cl, addr = s.accept()
-
     cl_file = cl.makefile('rwb', 0)
 
+    #Start reading
     config_ini=""
     content_length=0
 
     #Discard first line with different format
     line = cl_file.readline()
 
+    #Reading the header to get content length
     while True:
-
         line = cl_file.readline()
         if not line or line == b'\r\n':
             break
-
-        print(str(line))
         (header, value) = line.decode().split(":", 1)
-        print(header)
-        print(value)
         if header == "Content-Length":
             content_length=int(value)
-            print(content_length)
 
-
+    #Reading json file
     config_ini = cl_file.read(content_length).decode()
+
+    #Answer and closing socket
     response="HTTP/1.1 200 OK\r\nContent-Type: application/json\r\nContent-Length: 2\r\n\r\n{}"
     cl.send(response)
     cl.close()
@@ -63,13 +58,8 @@ def get_post():
 
 #Downloads the setups from the phone and returns them as a dictionnary
 def setup():
-    wifi_param={}
-
-    # get the HTTP post
     wifi_param_json=get_post()
-
     wifi_param=json.loads(wifi_param_json)
-
     return wifi_param
 
 #Creates the wifi
@@ -79,24 +69,16 @@ def wifi_init():
 
 # Tries to connect to the wifi
 def wifi_connect(ap,wifi_param, wlan):
-
     status=True
-
     ap.active(False)
     wlan.active(True)
 
+    #Try connection
     wlan.connect(wifi_param['ssid'], wifi_param['password'])
-    for i in range(1,10):
-        if not wlan.isconnected():
-            time.sleep(1)
-        else:
-            break
-
-
+    time.sleep(WIFI_TIMEOUT)
     if not wlan.isconnected():
         wlan.disconnect()
         status=False
-
     return status
 
 
@@ -110,19 +92,12 @@ def get_config(url):
     return config
 
 #Sends datas to the server, returns the configuration
-def send_datas(data,commands,url):
+def send_datas(url, data=None,commands=None):
     payload={"data":data}
-    if len(commands[0]) > 0:
+    if len(commands) > 0:
         payload["commands"] = commands
-    payload_json = json.dumps(payload)
-    print(payload_json)
-    response_json=requests.post(url, json=payload_json)
-    print(response_json.text)
+    response_json=requests.post(url, json=payload)
     response=json.loads(response_json.text)
-    print(response)
     return response
 
-def test():
-    ap=AP_activation()
-    config=get_post()
-    return  ap,config
+
